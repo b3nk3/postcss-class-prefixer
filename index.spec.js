a6559ba32aea679c5a9107c714add499e94f9d8a
Ben Szabo <me@benszabo.co.uk>
2021-02-12 04:03:43 +0000

    feat: amend tests   * increase test coverage

--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,50 @@
-const postcss = require("postcss");
-const plugin = require(".");
+const postcss = require('postcss');
+const plugin = require('.');
 
-async function run(input, output, opts = {}) {
+async function run(input, output, opts = {}, checkResults = true) {
   let result = await postcss([plugin(opts)]).process(input, {
     from: undefined,
   });
-  expect(result.css).toEqual(output);
-  expect(result.warnings()).toHaveLength(0);
+  if (checkResults) {
+    expect(result.css).toEqual(output);
+    expect(result.warnings()).toHaveLength(0);
+  }
 }
-describe("Class Perfixer tests", () => {
-  const prefixSelector = ".cms-content";
 
-  it("prefixes classes", async () => {
+describe('Class Perfixer tests', () => {
+  const prefixSelector = '.cms-content';
+
+  it('throws error if options object is missing/empty', async () => {
+    // const result = await ;
+    await expect(
+      run(`.class { a:10px; }`, `.class { a:10px; }`, false, {})
+    ).rejects.toThrowError(
+      'postcss-wrap-plugin: prefix option needs to be provided (eg.: `{ prefix: ".my-class-prefix"}` )'
+    );
+  });
+
+  it('throws error if prefix is not a string', async () => {
+    await expect(
+      run(
+        `.class { a:10px; }`,
+        `.class { a:10px; }`,
+        {
+          prefix: 10,
+        },
+        false
+      )
+    ).rejects.toThrowError(
+      'postcss-wrap-plugin: prefix option should be a string (eg.: ".my-class-prefix")'
+    );
+  });
+
+  it('prefixes classes', async () => {
     await run(`.class { a:10px; }`, `${prefixSelector} .class { a:10px; }`, {
       prefix: prefixSelector,
     });
   });
 
-  it("prefixes root tags", async () => {
+  it('prefixes root tags', async () => {
     await run(`body { a:10px; }`, `${prefixSelector} body { a:10px; }`, {
       prefix: prefixSelector,
     });
@@ -29,7 +56,7 @@ describe("Class Perfixer tests", () => {
     });
   });
 
-  it("prefixes element tags", async () => {
+  it('prefixes element tags', async () => {
     await run(`div { a:10px; }`, `${prefixSelector} div { a:10px; }`, {
       prefix: prefixSelector,
     });
@@ -45,5 +72,9 @@ describe("Class Perfixer tests", () => {
 
   it("skips IDs '#'", async () => {
     await run(`#id { a:10px; }`, `#id { a:10px; }`, { prefix: prefixSelector });
+  });
+
+  it('skips empty selectors', async () => {
+    await run(`{ a:10px; }`, `{ a:10px; }`, { prefix: prefixSelector });
   });
 });
