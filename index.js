210dc45b59ae03f915df3d7403f6de77ffdefbdc
Ben Szabo <me@benszabo.co.uk>
2021-02-12 04:02:19 +0000

    feat: add checks for options object

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
+const { isEmptyObject } = require('./utils/isEmptyObject');
+
 module.exports = (opts = {}) => {
   // Work with options here
+
   const { prefix } = opts;
 
-  const addWrapToSelector = (selector) => {
-    if (selector === "html") {
+  if (isEmptyObject(opts)) {
+    throw new Error(
+      'postcss-wrap-plugin: prefix option needs to be provided (eg.: `{ prefix: ".my-class-prefix"}` )'
+    );
+  }
+
+  if (typeof prefix !== 'string') {
+    throw new Error(
+      'postcss-wrap-plugin: prefix option should be a string (eg.: ".my-class-prefix")'
+    );
+  }
+
+  const addWrapToSelector = selector => {
+    if (selector === 'html') {
       return selector + prefix;
     }
     return `${prefix} ${selector}`;
   };
 
-  const wrapCSSSelector = (selector) => {
-    if (selector === "") {
+  const wrapCSSSelector = selector => {
+    if (selector === '') {
       return null;
     }
 
     return addWrapToSelector(selector);
   };
 
-  const wrapCssRuleSelector = (cssRuleSelector) => {
+  const wrapCssRuleSelector = cssRuleSelector => {
     return cssRuleSelector
-      .split(",")
-      .map((selector) => wrapCSSSelector(selector.trim()))
-      .join(", ");
+      .split(',')
+      .map(selector => wrapCSSSelector(selector.trim()))
+      .join(', ');
   };
 
-  const isRuleKeyframes = (rule) => {
+  const isRuleKeyframes = rule => {
     const { parent } = rule;
-    return parent.type === "atrule" && parent.name.includes("keyframes");
+    return parent.type === 'atrule' && parent.name.includes('keyframes');
   };
 
-  const isRuleId = (rule) => {
+  const isRuleId = rule => {
     const { selector } = rule;
-    return selector.includes("#");
+    return selector.includes('#');
   };
 
-  const checkIncludeRules = (rule) => {
+  const checkIncludeRules = rule => {
     // Don't prefix IDs
     if (isRuleId(rule)) {
       return false;
@@ -47,7 +62,7 @@ module.exports = (opts = {}) => {
   };
 
   return {
-    postcssPlugin: "postcss-class-prefixer",
+    postcssPlugin: 'postcss-class-prefixer',
     Rule(rule) {
       const { selector } = rule;
       if (checkIncludeRules(rule))
---
+++ utils/isEmptyObject.js
@@ -0,0 +1,1 @@
+exports.isEmptyObject = obj => !Object.entries(obj).length;
